5ec8b75f8af116e64f1e97110b262baf16b9568e
Giovanni <giolucarna@gmail.com>
2022-03-27 22:41:17 -0300

    :tada: First push!

---
+++ Conceptos Basicos/Ejercicio 1-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio_1_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+*
