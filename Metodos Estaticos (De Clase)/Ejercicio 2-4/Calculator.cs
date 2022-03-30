8de7d1190eea4d6f1fcff0d7b3530d7105d3ba7f
Giovanni <giolucarna@gmail.com>
2022-03-30 13:34:06 -0300

    :alembic: - Solved the excercise using methods and classes!

---
+++ Metodos Estaticos (De Clase)/Ejercicio 2-4/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_4_
+{
+    public class Calculator
+    {
+        public static int Calculate(int firstNumber, int secondNumber, char mathematicalOperationSymbol)
+        {
+            int operationResult;
+
+            operationResult = 0;
+
+            switch (mathematicalOperationSymbol)
+            {
+                case '+':
+                    operationResult = firstNumber + secondNumber;
+                    break;
+
+                case '-':
+                    operationResult = firstNumber - secondNumber;
+                    break;
+
+                case '*':
+                    operationResult = firstNumber * secondNumber;
+                    break;
+
+                case '/':
+
+                    while(!Validate(secondNumber))
+                    {
+                        Console.WriteLine("ERROR - You have to enter a number different than 0 to realize a division!");
+                        secondNumber = int.Parse(Console.ReadLine());
+                    }   
+
+                    operationResult = firstNumber / secondNumber;
+                    break;
+
+            }
+
+            return operationResult;
+        }
+
+        private static bool Validate(int secondNumber)
+        {
+            bool validation = false;
+
+            if(secondNumber != 0)
+            {
+                validation = true;
+            }
+
+            return validation;
+        }
+    }
+}
---
+++ Metodos Estaticos (De Clase)/Ejercicio 2-4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Classes_Library;
+
+namespace Ejercicio_2_4_
+{
+    /*
+     *  Consigna
+        Realizar un programa que permita realizar operaciones matemáticas simples (suma, resta, multiplicación y división).
+        Crear una clase llamada Calculadora que posea dos métodos estáticos (de clase):
+        Calcular (público): Recibirá tres parámetros, el primer operando, el segundo operando y la operación matemática. El método devolverá el resultado de la operación.
+        Validar (privado): Recibirá como parámetro el segundo operando. Este método se debe utilizar sólo cuando la operación elegida sea la DIVISIÓN.
+        Este método devolverá true si el operando es distinto de cero.
+        Se le debe pedir al usuario que ingrese dos números y la operación que desea realizar (ingresando el caracter +, -, * o /).
+        El usuario decidirá cuándo finalizar el programa.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            char mathematicalSymbol;
+            int result;
+            char continueCalculating;
+
+            do
+            {
+                Console.WriteLine("Enter the first number to do a mathematical operation!");
+                while (!int.TryParse(Console.ReadLine(), out firstNumber)) // !Validación de numeros || Validación de cadena
+                {
+                    Console.WriteLine("ERROR! You have entered something different than a number, RE-Enter it: ");
+                }
+
+                Console.WriteLine("Enter the second number to do a mathematical operation!");
+                while (!int.TryParse(Console.ReadLine(), out secondNumber)) // !Validación de numeros || Validación de cadena
+                {
+                    Console.WriteLine("ERROR! You have entered something different than a number, RE-Enter it: ");
+                }
+                Console.WriteLine("Enter the symbol of which mathematical operation you want to do!");
+                while (!char.TryParse(Console.ReadLine(), out mathematicalSymbol)) // !Validación de numeros || Validación de cadena
+                {
+                    Console.WriteLine("ERROR! You have entered something different than a mathemathical symbol, RE-Enter it: ");
+                }
+
+                result = Calculator.Calculate(firstNumber, secondNumber, mathematicalSymbol);
+
+                Console.WriteLine("The result is {0}", result);
+
+                Console.WriteLine("You want to keep making use of the calculator? S to continue or whatever to stop!");
+                continueCalculating = char.Parse(Console.ReadLine());
+
+            }while(Validator.ValidateAnswer(continueCalculating));
+
+            
+        }
+    }
+}
