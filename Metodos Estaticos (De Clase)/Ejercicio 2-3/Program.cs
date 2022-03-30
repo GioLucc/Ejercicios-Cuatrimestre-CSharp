c6290c0df7c900c62a80503a8695c78b4eaa3e76
Giovanni <giolucarna@gmail.com>
2022-03-30 13:31:58 -0300

    :tada: Added the 2-3 excersice for it's future solution!

---
+++ Metodos Estaticos (De Clase)/Ejercicio 2-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_2_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
