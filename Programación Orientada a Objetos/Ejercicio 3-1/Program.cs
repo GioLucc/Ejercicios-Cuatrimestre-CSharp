36aa5e897d128803c4917dd43d293bb8cde97c3a
Giovanni <giolucarna@gmail.com>
2022-04-04 00:04:35 -0300

    Feat - :tada: Used OOP and constructors on this excercise!

---
+++ Programación Orientada a Objetos/Ejercicio 3-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ClassLibrary;
+
+namespace Ejercicio_3_1_
+{
+    class Program
+    {
+        /*Crear una aplicación de consola y una biblioteca de clases que contenga la clase Cuenta.
+
+        Deberá tener los atributos:
+
+        titular que contendrá la razón social del titular de la cuenta.
+        cantidad que será un número decimal que representa al monto actual de dinero en la cuenta.
+        Construir los siguientes métodos para la clase:
+
+        Un constructor que permita inicializar todos los atributos.
+        Un método getter para cada atributo.
+        mostrar retornará una cadena de texto con todos los datos de la cuenta.
+        ingresar recibirá un monto para acreditar a la cuenta, Si el monto ingresado es negativo, no se hará nada.
+        retirar recibirá un monto para debitar de la cuenta.La cuenta puede quedar en negativo.
+        En el método Main, simular depósitos y extracciones de dinero de la cuenta, e ir mostrando como va variando el saldo.
+        */
+
+        static void Main(string[] args)
+        {
+            Cuenta thiagoMejillas = new Cuenta("Gatos asociados", 1500);
+            string razonSocialDeThiago;
+            decimal dineroEnCuentaDeThiago;
+
+            thiagoMejillas.Mostrar();
+
+
+            razonSocialDeThiago = thiagoMejillas.GetRazonSocial();
+            dineroEnCuentaDeThiago =  thiagoMejillas.GetDineroEnCuenta();
+
+            Console.WriteLine("Razon social de Thiago: " + razonSocialDeThiago + "Dinero en cuenta de Thiago: " + dineroEnCuentaDeThiago); ;
+
+            
+
+        }
+    }
+}
